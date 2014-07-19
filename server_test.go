80b1ab0d41f9dca8f38b7c4c5d1f0433fa53a3f1
Kornel Maczyński <kornel661@gmail.com>
2014-07-19 23:10:17 +0100

    remove race condition from tests from the "language level"

--- server_test.go
+++ server_test.go
@@ -1,58 +1,76 @@
 package manners
 
 import (
+	"net"
 	"net/http"
 	"testing"
+	"time"
 )
 
 // Tests that the server allows in-flight requests to complete before shutting
 // down.
 func TestGracefulness(t *testing.T) {
-	ready := make(chan bool)
-	done := make(chan bool)
+	serverDone := make(chan bool)
+	clientDone := make(chan bool)
+	clientConnected := make(chan bool)
 
-	exited := false
-
-	handler := newBlockingHandler(ready, done)
 	server := NewServer()
 
+	// server
 	go func() {
-		err := server.ListenAndServe(":7000", handler)
+		err := server.ListenAndServe("localhost:7000", nil)
 		if err != nil {
 			t.Error(err)
 		}
-
-		exited = true
+		serverDone <- true
 	}()
 
+	// client
 	go func() {
-		_, err := http.Get("http://localhost:7000")
+		// give the server a chance to start (ugly, FIXME)
+		time.Sleep(1000 * time.Microsecond)
+
+		conn, err := net.Dial("tcp", "localhost:7000")
 		if err != nil {
 			t.Error(err)
 		}
+		defer conn.Close()
+
+		// signal that client established connection
+		clientConnected <- true
+
+		// give the server a chance to exit ungracefully
+		time.Sleep(1000 * time.Microsecond)
+
+		// signal we're about to exit
+		clientDone <- true
 	}()
 
-	// This will block until the server is inside the handler function.
-	<-ready
+	// wait for client to connect
+	<-clientConnected
+
+	// green light for server to shutdown
 	server.Shutdown <- true
 
-	if exited {
+	// let's see who exits first
+	select {
+	case <-clientDone:
+		// client exited first, test passed
+		// allow server to be done
+		<-serverDone
+	case <-serverDone:
 		t.Fatal("The request did not complete before server exited")
-	} else {
-		// The handler is being allowed to run to completion; test passes.
 	}
-	<-done
 }
 
-// Tests that the server begins to shut down when it receives a SIGINT and
-// does not accept new requests
+// Tests that the server begins to shut down when told to and does not accept
+// new requests
 func TestShutdown(t *testing.T) {
-	handler := newTestHandler()
 	server := NewServer()
 	exited := make(chan bool)
 
 	go func() {
-		err := server.ListenAndServe(":7100", handler)
+		err := server.ListenAndServe("localhost:7100", nil)
 		if err != nil {
 			t.Error(err)
 		}
@@ -63,6 +81,7 @@ func TestShutdown(t *testing.T) {
 
 	<-exited
 	_, err := http.Get("http://localhost:7100")
+
 	if err == nil {
 		t.Fatal("Did not receive an error when trying to connect to server.")
 	}
