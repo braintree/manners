9a76801062a82d30655382b2792421cb781be752
Tam Nguyen <tamnvt@hotmail.com>
2018-10-28 17:15:50 +0700

    Add examples

---
+++ examples/simple/example.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/braintree/manners"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var GracefulServer *manners.GracefulServer
+
+func main() {
+	router := http.NewServeMux()
+	router.Handle("/", indexHandler())
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		GracefulServer.Close()
+		close(idleConnsClosed)
+	}()
+
+	GracefulServer = manners.NewWithServer(&http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	})
+
+	log.Fatal(GracefulServer.ListenAndServe())
+
+	<-idleConnsClosed
+}
+
+func indexHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(5*time.Second)
+		w.Write([]byte("Hello"))
+	})
+}
